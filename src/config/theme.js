1ef356af03ec7e7f2060413d276f9173181fc744
imamsutono <imamssutono@gmail.com>
2018-02-21 20:17:22 +0700

    Add theme.js for color theming

---
+++ src/config/theme.js
@@ -0,0 +1,22 @@
+const blue = {
+  bgScreen: '#42A5F5',
+  bgInput: '#64B5F6',
+  clrLabel: '#1E88E5',
+  clrPlaceholder: 'rgba(255, 255, 255, 0.5)'
+};
+
+const green = {
+  bgScreen: '#00E676',
+  bgInput: '#66FFA6',
+  clrLabel: '#00B248',
+  clrPlaceholder: '#B9F6CA'
+};
+
+const red = {
+  bgScreen: '#FF3D00',
+  bgInput: '#FF9E80',
+  clrLabel: '#C30000',
+  clrPlaceholder: '#FFD0B0'
+};
+
+export { blue, green, yellow, red };
